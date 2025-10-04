a8d15a26e4d603ef9b1dc4f10d4aca0f4fcaf74a
Raunak Ray <raunakay3260@gmail.com>
2025-10-04 19:40:14 +0530

    Added Custom Api response and errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectToDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
 
-connectToDB();
-const app = express();
-
-const port = process.env.PORT;
-
-app.listen(port, () => {
-    console.log(`App is running on ${port}`);
-});
+connectToDB()
+    .then(() => {
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`Server is listening on Port: ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("Failed to connect to database ! ", error);
+    });
